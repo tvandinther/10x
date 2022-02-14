15f4531fa68dd945351e27d741793c1189fe711f
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2022-02-14 14:35:20 +1300

    added p10k theme

--- install.sh
+++ install.sh
@@ -10,3 +10,8 @@ if ! [[ $SHELL == */zsh ]]; then
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Get Theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's/ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' $HOME/.zshrc
+
